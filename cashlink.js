66d2ac8f5cf73619e3cd7046e81078c879444a55
pb <mail@paberr.net>
2017-07-02 23:31:58 +0200

    rewrite to ES6

--- cashlink.js
+++ cashlink.js
@@ -9,77 +9,92 @@ class Cashlink extends Nimiq.Observable {
 	}
 
 
-	static async createCashlink($, amount, fee = 0) {
-		let transferWallet = await Nimiq.Wallet.createVolatile();
-		let cashlink = new Cashlink($, transferWallet);
-		if (!Nimiq.NumberUtils.isUint64(amount) || amount===0) {
-			// all amounts and fees are always integers to ensure floating point precision.
-			throw Error("Only positive integer amounts allowed.");
-		}
-		if (!Nimiq.NumberUtils.isUint64(fee) || fee>=amount) {
-			throw Error("Illegal fee.");
-		}
-		let balance = await $.accounts.getBalance($.wallet.address);
-		if (balance.value < amount) {
-			throw Error("You can't send more money than you own");
-		}
-		// we do amount-fee because the recipient has to pay the fee
-		let transaction = await $.wallet.createTransaction(transferWallet.address, amount-fee, fee, balance.nonce);
-		await $.mempool.pushTransaction(transaction);
-		return cashlink;
+	static createCashlink($, amount, fee = 0) {
+		return Nimiq.Wallet.createVolatile().then(transferWallet => {
+			let cashlink = new Cashlink($, transferWallet);
+			if (!Nimiq.NumberUtils.isUint64(amount) || amount===0) {
+				// all amounts and fees are always integers to ensure floating point precision.
+				throw Error("Only positive integer amounts allowed.");
+			}
+			if (!Nimiq.NumberUtils.isUint64(fee) || fee>=amount) {
+				throw Error("Illegal fee.");
+			}
+
+			return $.accounts.getBalance($.wallet.address).then(balance => {
+				if (balance.value < amount) {
+					throw Error("You can't send more money than you own");
+				}
+				// we do amount-fee because the recipient has to pay the fee
+				return $.wallet.createTransaction(transferWallet.address, amount-fee, fee, balance.nonce).then(transaction => {
+					return $.mempool.pushTransaction(transaction).then(() => {
+						return cashlink;
+					});
+				});
+			});
+		});
 	}
 
 
-	static async decodeCashlink($, url) {
+	static decodeCashlink($, url) {
 		let urlParts = url.split('#');
 		if (urlParts[0].indexOf(Cashlink.BASE_URL)===-1) {
 			throw Error("Not a valid cashlink.");
 		}
 		let privateKey = Nimiq.PrivateKey.unserialize(BufferUtils.fromBase64(urlParts[1]));
-		let keyPair = await Nimiq.KeyPair.derive(privateKey);
-		let transferWallet = await new Nimiq.Wallet(keyPair);
-		return new Cashlink($, transferWallet);
+		return Nimiq.KeyPair.derive(privateKey).then(keyPair => {
+			return new Nimiq.Wallet(keyPair).then(transferWallet => {
+				return new Cashlink($, transferWallet);
+			});
+		});
 	}
 
 
-	async accept(fee = 0) {
+	accept(fee = 0) {
 		// get out the money. Only the confirmed amount, because we can't request unconfirmed money.
-		let balance = await this.$.accounts.getBalance(this._transferWallet.address);
-		if (balance.value === 0) {
-			throw Error('There is no confirmed balance in this link');
-		}
-		let transaction = await this._transferWallet.createTransaction(this.$.wallet.address,
-			balance.value-fee, fee, balance.nonce);
-		await this.$.mempool.pushTransaction(transaction);
-	}
-
-
-	async getAmount(includeUnconfirmed) {
-		let balance = (await this.$.accounts.getBalance(this._transferWallet.address)).value;
-		if (includeUnconfirmed) {
-			let transferWalletAddress = this._transferWallet.address;
-			await this.$.mempool._evictTransactions(); // ensure that already validated transactions are ignored
-			let transactions = Object.values(this.$.mempool._transactions);
-			for (let i=0; i<transactions.length; ++i) {
-				let transaction = transactions[i];
-				let senderPubKey = transaction.senderPubKey;
-				let recipientAddr = transaction.recipientAddr;
-				if (recipientAddr.equals(transferWalletAddress)) {
-					// money sent to the transfer wallet
-					balance += transaction.value;
-				} else if (senderPubKey.equals(this._transferWallet.publicKey)) {
-					balance -= transaction.value + transaction.fee;
-				}
+		return this.$.accounts.getBalance(this._transferWallet.address).then(balance => {
+			if (balance.value === 0) {
+				throw Error('There is no confirmed balance in this link');
 			}
-		}
-		return balance;
+			return this._transferWallet.createTransaction(this.$.wallet.address, balance.value-fee, fee, balance.nonce).then(transaction => {
+				return this.$.mempool.pushTransaction(transaction);
+			});
+		});
 	}
 
 
-	async _onTransactionAdded(transaction) {
+	getAmount(includeUnconfirmed) {
+		return this.$.accounts.getBalance(this._transferWallet.address).then(res => {
+			let balance = res.value;
+			if (includeUnconfirmed) {
+				let transferWalletAddress = this._transferWallet.address;
+				return this.$.mempool._evictTransactions().then(() => {
+					// ensure that already validated transactions are ignored
+					let transactions = Object.values(this.$.mempool._transactions);
+					for (let i=0; i<transactions.length; ++i) {
+						let transaction = transactions[i];
+						let senderPubKey = transaction.senderPubKey;
+						let recipientAddr = transaction.recipientAddr;
+						if (recipientAddr.equals(transferWalletAddress)) {
+							// money sent to the transfer wallet
+							balance += transaction.value;
+						} else if (senderPubKey.equals(this._transferWallet.publicKey)) {
+							balance -= transaction.value + transaction.fee;
+						}
+					}
+					return balance;
+				});
+			}
+			return balance;
+		});
+	}
+
+
+	_onTransactionAdded(transaction) {
 		if (transaction.recipientAddr.equals(this._transferWallet.address)
 			|| (transaction.senderPubKey).equals(this._transferWallet.publicKey)) {
-			this.fire('unconfirmed-amount-changed', await this.getAmount(true));
+			return this.getAmount(true).then(val => {
+				this.fire('unconfirmed-amount-changed', val);
+			});
 		}
 	}
 
@@ -93,4 +108,4 @@ class Cashlink extends Nimiq.Observable {
 		return Cashlink.BASE_URL + '#' + Nimiq.BufferUtils.toBase64(this._transferWallet.keyPair.privateKey.serialize());
 	}
 }
-Cashlink.BASE_URL = 'nimiq.com/receive';
+Cashlink.BASE_URL = 'nimiq.com/cashlinks';
